0fc1e30d389ce0dde124f5f719c53caaff252cf1
Tiefen-boop <roi.tief97@gmail.com>
2021-04-27 22:23:21 +0300

    Rebuilt Polynomial with ArrayList

--- src/Polymath/Polynomial.java
+++ src/Polymath/Polynomial.java
@@ -1,79 +1,69 @@
 package Polymath;
 
-import java.util.Vector;
+import java.util.ArrayList;
 
 public class Polynomial {
     //fields
-    private Monomial[] monomials; //array of monomials where monomials[i] = Monomial with exponent i
-    //also - degree of the polynomial p will be (p.monomials.length - 1)
+    private ArrayList<Monomial> monomials; //array of monomials where monomials.get(i) = Monomial with exponent i
+    //also - degree of the polynomial p will be (p.monomials.size() - 1)
 
     //constructors
-    public Polynomial(int numOfMonomials){
-        if (numOfMonomials <= 0)
-            numOfMonomials = 1;
-        monomials = new Monomial[numOfMonomials];
-        resetData();
-    }
-
-    public Polynomial(Monomial[] monomials){ //we expect monomials[i] = Monomial with exponent i & not null
-        if (monomials == null)
-            throw new NullPointerException("Monomials can't be null array");
-        this.monomials = new Monomial[monomials.length]; //preparing to copy the argument
-        for (int i = 0; i < monomials.length; i++) {
-            if (monomials[i] == null || monomials[i].getExponent() != i) //array not as expected
-                throw new IllegalArgumentException("For each monomial in the array monomials - monomial[i] has to be a monomial with exponent i");
-            this.monomials[i] = new Monomial(monomials[i]);
-        }
-        if (this.monomials.length == 0)
-            this.monomials = new Monomial[]{new Monomial(0,0)};
+    public Polynomial(ArrayList<Monomial> monomials){
+        checkMonomials(monomials);
+        monomials = new ArrayList<Monomial>(monomials); //copying the given list to ensure immutability of Polynomial
     }
 
     //copy constructor
     public Polynomial(Polynomial p){
-        monomials = new Monomial[p.monomials.length];
-        for (int i = 0 ; i < p.monomials.length ; i++) {
-            monomials[i] = new Monomial(p.monomials[i]);
-        }
+        monomials = new ArrayList<Monomial>(p.monomials);
     }
 
     //methods
 
-    //resets all monomials to monomials with coefficient 0
-    private void resetData(){
-        for (int i = 0; i < monomials.length; i++) {
-            monomials[i] = new Monomial(0,i);
+    //checks legality of monomials
+    public void checkMonomials(ArrayList<Monomial> monomials){ //we expect monomials.get(i) = Monomial with exponent i
+        if (monomials == null)
+            throw new NullPointerException("Monomials can't be null");
+        int i = 0;
+        for (Monomial monom: monomials){
+            if (monom == null)
+                throw new NullPointerException("Monomial at index - " + i + " is null");
+            if (monom.getExponent() != i)
+                throw new IllegalArgumentException("Monomial's exponent at index - " + i + " is not - " + i);
         }
-    }
-
-    //adds monomial to the polynomial
-    private void addMonomial(Monomial m){
-        int i = m.getExponent(); //index of exponent
-        monomials[i] = monomials[i].add(m);
     }
 
     //adds p to the polynomial
     public Polynomial add(Polynomial p){
-        Polynomial addition = new Polynomial(Math.max(monomials.length, p.monomials.length));
-        for (int i = 0 ; i < addition.monomials.length ; i++){ //iterating through the exponents
-            //checking for each polynomial if it has an monomial with exponent i
-            //adding the monomial if it does
-            if (i < monomials.length)
-                addition.addMonomial(monomials[i]);
-            if (i < p.monomials.length)
-                addition.addMonomial(p.monomials[i]);
+        int size = Math.max(this.monomials.size() , p.monomials.size()); //size of the new Polynomial will be the size of the largest of the poynomials (and thus have the same degree)
+        ArrayList<Monomial> monomials = new ArrayList<Monomial>(size);
+        for (int i = 0 ; i < size; i++){ //iterating through the exponents
+            if ((this.monomials.size() > i ) & (p.monomials.size() > i)) //if both polynomials have monomial with exponent i
+                monomials.add(this.monomials.get(i).add(p.monomials.get(i))); //we insert the addition
+            //if not, one polynomial will and we'll insert the monomial as is
+            else if (monomials.size() > i)
+                monomials.add(this.monomials.get(i));
+            else
+                monomials.add(p.monomials.get(i));
         }
-        return addition;
+        return new Polynomial(monomials);
     }
 
     //multiplies p with the polynomial
     public Polynomial mul(Polynomial p){
         //From https://en.wikipedia.org/wiki/Degree_of_a_polynomial
         //for Polynomials p, q - deg(p*q) = deg(p) + deg(q)
-        Polynomial multiplication = new Polynomial((monomials.length-1)+(p.monomials.length-1));
-        for (Monomial thisMonom: monomials)
-            for (Monomial pMonom: p.monomials)
-                multiplication.addMonomial(thisMonom.mult(pMonom));
-            return multiplication;
+        ArrayList<Monomial> monomials = new ArrayList<Monomial>((this.monomials.size()-1)+(p.monomials.size()-1) +1);
+        for (Monomial thisMonom: this.monomials)
+            for (Monomial pMonom: p.monomials) {
+                Monomial multiplication = thisMonom.mult(pMonom);
+                int exponent = multiplication.getExponent();
+                if (monomials.size() <= exponent) //if we didn't add that exponent yes
+                    monomials.add(multiplication); //just add it to the list
+                else //if such exponent exists
+                    monomials.set(exponent, monomials.get(exponent).add(multiplication)); //we need to add the monomials
+            }
+            return new Polynomial(monomials);
     }
 
     //evaluates the polynomial with s
@@ -86,41 +76,32 @@ public class Polynomial {
 
     //derivates the polynomial
     public Polynomial derivative(){
-        Monomial[] monomials = new Monomial[this.monomials.length-1];
-        for (int i = 1; i < this.monomials.length; i++) //derivating monomial with exponent 0 will return 0 so we can skip it
-            monomials[i-1] = this.monomials[i].derivative();
+        ArrayList<Monomial> monomials = new ArrayList<Monomial>(this.monomials.size()-1);
+        for (int i = 1; i < this.monomials.size(); i++) //derivating monomial with exponent 0 will return 0 so we can skip it
+            monomials.add(this.monomials.get(i).derivative());
         return new Polynomial(monomials);
     }
 
     public String toString(){
         String str = "";
-        for (int i = 0; i < monomials.length; i++) {
-            String monomial = monomials[i].toString();
-            if (!monomial.equals("0"))
-                str += monomial + " ";
-        }
+        for (Monomial monom: monomials)
+            if (!monom.toString().equals("0"))
+                str += monom.toString();
         if (str.length() == 0)
             str = "0";
-        return str.trim();
+        return str;
     }
 
     //static functions
 
     public static Polynomial build(String input){
-        Monomial[] monomials = convertString(input.trim().replaceAll(" +", " "), 0); //cleaning input of all unwanted spaces before calling convertString
-        return new Polynomial(monomials);
-    }
-
-    private static Monomial[] convertString(String str, int exponent){
-        int index = str.indexOf(' '); //will tell us if we have multiple arguments or not
-        if (index > 0) { //multiple arguments
-            Monomial[] monomials = convertString(str.substring(index+1), exponent+1);
-            monomials[exponent] = new Monomial(str.substring(0, index), exponent);
-            return monomials;
+        input = input.trim().replaceAll(" +", " "); //cleaning input of all unwanted spaces
+        ArrayList<Monomial> monomials = new ArrayList<Monomial>();
+        int exponent = 0;
+        for (String coef: input.split(" ")){
+            monomials.add(new Monomial(coef, exponent));
+            exponent++;
         }
-        //single argument
-        Monomial[] monomials = new Monomial[exponent+1];
-        monomials[exponent] = new Monomial(str, exponent);
-        return monomials;
+        return new Polynomial(monomials);
     }
 }
